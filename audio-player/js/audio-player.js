cdfd0516cc675377e1d12ca3a7f324c1f8bbfe5b
Alv0425 <vilejshikova@gmail.com>
2023-09-23 23:21:58 +0300

    refactor: apply prettier and stylelint

--- audio-player/js/audio-player.js
+++ audio-player/js/audio-player.js
@@ -1,5 +1,4 @@
-'use strict';
-let playlistIndexes= [0,1,2,3];
+"use strict";
 let currentMusuic = 0;
 let currentDuration = 0;
 let isPlayingNow = false;
@@ -9,25 +8,25 @@ let currentVolume = 0.7;
 
 const trackItems = [];
 tracks.forEach((track, index) => {
-  const playlistItem = document.createElement('li');
-  const playlistItemButton = document.createElement('button');
-  playlistItemButton.className = 'track__button';
+  const playlistItem = document.createElement("li");
+  const playlistItemButton = document.createElement("button");
+  playlistItemButton.className = "track__button";
   playlistItem.append(playlistItemButton);
-  const playlistTrackName = document.createElement('span');
-  playlistTrackName.className = 'track-name';
-  const playlistTrackTitle = document.createElement('span');
-  playlistTrackTitle.className = 'track__title';
-  const playlistTrackAuthor = document.createElement('span');
-  playlistTrackAuthor.className = 'track__author';
-  const playlistTrackTime = document.createElement('span');
-  playlistTrackTime.className = 'track__time';
+  const playlistTrackName = document.createElement("span");
+  playlistTrackName.className = "track-name";
+  const playlistTrackTitle = document.createElement("span");
+  playlistTrackTitle.className = "track__title";
+  const playlistTrackAuthor = document.createElement("span");
+  playlistTrackAuthor.className = "track__author";
+  const playlistTrackTime = document.createElement("span");
+  playlistTrackTime.className = "track__time";
   playlistItemButton.append(playlistTrackName, playlistTrackTime);
   playlistTrackName.append(playlistTrackTitle, playlistTrackAuthor);
   playlistTrackTitle.textContent = track.title;
   playlistTrackAuthor.textContent = track.author;
   playlistList.append(playlistItem);
   trackItems.push(playlistItem);
-  playlistItemButton.addEventListener('click', () => {
+  playlistItemButton.addEventListener("click", () => {
     currentMusuic = index;
     openTrack(currentMusuic);
     currTime = 0;
@@ -38,20 +37,22 @@ tracks.forEach((track, index) => {
 openTrack(0);
 
 srcs.forEach((src, index) => {
-  const audio = new Audio;
-  audio.preload = 'auto';
+  const audio = new Audio();
+  audio.preload = "auto";
   audio.src = src;
-  audio.onloadedmetadata = () => { 
-    trackItems[index].childNodes[0].childNodes[1].textContent = getCurrentTime(audio.duration);
+  audio.onloadedmetadata = () => {
+    trackItems[index].childNodes[0].childNodes[1].textContent = getCurrentTime(
+      audio.duration
+    );
   };
 });
 
-function setCoverImage(src) {  
+function setCoverImage(src) {
   const image = new Image();
   image.src = src;
-  image.onload = () => {      
+  image.onload = () => {
     cover.style.backgroundImage = `url(${src})`;
-  }; 
+  };
 }
 
 function openTrack(index) {
@@ -64,14 +65,20 @@ function openTrack(index) {
   body.style.backgroundColor = tracks[index].color;
   setCoverImage(tracks[index].cover);
   trackTitle.textContent = tracks[index].title;
-  trackAuthor.textContent = tracks[index].author; 
-  trackItems.forEach((item) => {item.className = 'track'});
-  trackItems[index].classList.add('track-active');
-  music.addEventListener('loadedmetadata', function metadataLoad(event) {
-    timeFull.textContent = getCurrentTime(music.duration);
-    currentDuration = music.duration;
-    event.target.removeEventListener('loadedmetadata', metadataLoad);
-  }, false);
+  trackAuthor.textContent = tracks[index].author;
+  trackItems.forEach((item) => {
+    item.className = "track";
+  });
+  trackItems[index].classList.add("track-active");
+  music.addEventListener(
+    "loadedmetadata",
+    function metadataLoad(event) {
+      timeFull.textContent = getCurrentTime(music.duration);
+      currentDuration = music.duration;
+      event.target.removeEventListener("loadedmetadata", metadataLoad);
+    },
+    false
+  );
 }
 
 function playMusic() {
@@ -79,11 +86,11 @@ function playMusic() {
     music.currentTime = currTime;
     music.play();
     isPlayingNow = true;
-    controlPlayPause.classList.remove('control-play');
-    controlPlayPause.classList.add('control-pause');
-    backgroundBulb.classList.add('blub-rotate');
-    backgroundBulb2.classList.add('blub-rotate');
-    backgroundBulb3.classList.add('blub-rotate');
+    controlPlayPause.classList.remove("control-play");
+    controlPlayPause.classList.add("control-pause");
+    backgroundBulb.classList.add("blub-rotate");
+    backgroundBulb2.classList.add("blub-rotate");
+    backgroundBulb3.classList.add("blub-rotate");
   }
 }
 
@@ -91,15 +98,15 @@ function pauseMusic() {
   if (isPlayingNow) {
     music.pause();
     isPlayingNow = false;
-    controlPlayPause.classList.add('control-play');
-    controlPlayPause.classList.remove('control-pause');
-    backgroundBulb.classList.remove('blub-rotate');
-    backgroundBulb2.classList.remove('blub-rotate');
-    backgroundBulb3.classList.remove('blub-rotate');
+    controlPlayPause.classList.add("control-play");
+    controlPlayPause.classList.remove("control-pause");
+    backgroundBulb.classList.remove("blub-rotate");
+    backgroundBulb2.classList.remove("blub-rotate");
+    backgroundBulb3.classList.remove("blub-rotate");
   }
 }
 
-controlPlayPause.addEventListener('click', () => {
+controlPlayPause.addEventListener("click", () => {
   if (isPlayingNow) {
     pauseMusic();
   } else {
@@ -108,7 +115,7 @@ controlPlayPause.addEventListener('click', () => {
 });
 
 setInterval(() => {
-  progressRange.value = music.currentTime / music.duration * 100;
+  progressRange.value = (music.currentTime / music.duration) * 100;
   progressCurrent.style.width = progressRange.value + "%";
   timeCurrent.textContent = getCurrentTime(music.currentTime);
   currTime = music.currentTime;
@@ -117,7 +124,10 @@ setInterval(() => {
       playNext();
     } else {
       let randomIndex = Math.floor(Math.random() * tracks.length);
-      currentMusuic = randomIndex !== currentMusuic ? randomIndex : Math.floor(Math.random() * 4);
+      currentMusuic =
+        randomIndex !== currentMusuic
+          ? randomIndex
+          : Math.floor(Math.random() * 4);
       setTimeout(() => {
         openTrack(currentMusuic);
         isPlayingNow = false;
@@ -127,7 +137,7 @@ setInterval(() => {
   }
 }, 500);
 
-function playNext(){
+function playNext() {
   if (currentMusuic < tracks.length - 1) {
     currentMusuic += 1;
     openTrack(currentMusuic);
@@ -145,80 +155,83 @@ function playNext(){
   }
 }
 
-function playPrev(){
+function playPrev() {
   if (currentMusuic > 0) {
     currentMusuic -= 1;
     openTrack(currentMusuic);
-    setTimeout(()=>{
+    setTimeout(() => {
       isPlayingNow = false;
       playMusic();
     }, 100);
   } else {
     currentMusuic = tracks.length - 1;
     openTrack(currentMusuic);
-    setTimeout(()=>{
+    setTimeout(() => {
       isPlayingNow = false;
       playMusic();
     }, 100);
   }
 }
 
-controlNext.addEventListener('click', () => {playNext();});
-controlPrev.addEventListener('click', () => {playPrev();});
+controlNext.addEventListener("click", () => {
+  playNext();
+});
+controlPrev.addEventListener("click", () => {
+  playPrev();
+});
 
-progressRange.addEventListener('input', () => {
-  currTime = progressRange.value * music.duration / 100;
+progressRange.addEventListener("input", () => {
+  currTime = (progressRange.value * music.duration) / 100;
   music.currentTime = currTime;
 });
 
-controlRepeatShuffle.addEventListener('click', () => {
+controlRepeatShuffle.addEventListener("click", () => {
   if (isSorted) {
     isSorted = false;
-    controlRepeatShuffle.classList.remove('control-repeat');
-    controlRepeatShuffle.classList.add('control-shuffle');
+    controlRepeatShuffle.classList.remove("control-repeat");
+    controlRepeatShuffle.classList.add("control-shuffle");
   } else {
     isSorted = true;
-    controlRepeatShuffle.classList.add('control-repeat');
-    controlRepeatShuffle.classList.remove('control-shuffle');
+    controlRepeatShuffle.classList.add("control-repeat");
+    controlRepeatShuffle.classList.remove("control-shuffle");
   }
 });
 
 document.addEventListener("keyup", (event) => {
-  if (event.code == 'Space'){
+  if (event.code == "Space") {
     if (isPlayingNow) {
       setTimeout(() => {
         pauseMusic();
       }, 10);
-      
     } else {
       setTimeout(() => {
         playMusic();
       }, 10);
     }
   }
-  if (event.key == 'ArrowUp'){
+  if (event.key == "ArrowUp") {
     setTimeout(() => {
       playPrev();
     }, 500);
   }
-  if (event.key == 'ArrowDown'){
+  if (event.key == "ArrowDown") {
     setTimeout(() => {
       playNext();
     }, 500);
   }
 });
 
-volumeBar.addEventListener('input', () => {
+volumeBar.addEventListener("input", () => {
   currentVolume = volumeBar.value / 100;
   music.volume = currentVolume;
 });
 
-controlVolume.addEventListener('click', () => {
+controlVolume.addEventListener("click", () => {
   if (!music.muted) {
     music.muted = true;
-    controlVolume.classList.add('control-volume-muted');
+    controlVolume.classList.add("control-volume-muted");
   } else {
     music.muted = false;
-    controlVolume.classList.remove('control-volume-muted');
+    controlVolume.classList.remove("control-volume-muted");
   }
 });
--- audio-player/js/constants.js
+++ audio-player/js/constants.js
@@ -1,230 +1,244 @@
-'use strict';
+"use strict";
 const tracks = [
   {
-    'title':'Have you ever seen the rain',
-    'author':'Creedence Clearwater Revival',
-    'src':'./audio/have-you-ever-seen-the-rain-ccr.mp3',
-    'cover':'./images/ccr-have-you-ever.jpg',
-    'color':'#28395A'
+    title: "Makeba",
+    author: "Jain",
+    src: "./audio/makeba-jain.mp3",
+    cover: "./images/jain.jfif",
+    color: "#F9C610",
   },
   {
-    'title':'Through the glass',
-    'author':'Stone sour',
-    'src':'./audio/stone-sour-through-the-glass.mp3',
-    'cover':'./images/stone-sour.jpeg',
-    'color':'#C5D5EA'
+    title: "Have you ever seen the rain",
+    author: "Creedence Clearwater Revival",
+    src: "./audio/have-you-ever-seen-the-rain-ccr.mp3",
+    cover: "./images/ccr-have-you-ever.jpg",
+    color: "#28395A",
   },
   {
-    'title':'Eye of the tiger',
-    'author':'Survivor',
-    'src':'./audio/eye-of-the-tiger-survivor.mp3',
-    'cover':'./images/survivor.jpg',
-    'color':'#180501'
+    title: "Through the glass",
+    author: "Stone sour",
+    src: "./audio/stone-sour-through-the-glass.mp3",
+    cover: "./images/stone-sour.jpeg",
+    color: "#C5D5EA",
   },
   {
-    'title':'Makeba',
-    'author':'Jain',
-    'src':'./audio/makeba-jain.mp3',
-    'cover':'./images/jain.jfif',
-    'color':'#F9C610'
+    title: "Eye of the tiger",
+    author: "Survivor",
+    src: "./audio/eye-of-the-tiger-survivor.mp3",
+    cover: "./images/survivor.jpg",
+    color: "#180501",
   },
   {
-    'title':'Fortunate son',
-    'author':'Creedence Clearwater Revival',
-    'src':'./audio/fortunate-son-ccr.mp3',
-    'cover':'./images/fortunate-son.jfif',
-    'color':'#180501'
+    title: "Fortunate son",
+    author: "Creedence Clearwater Revival",
+    src: "./audio/fortunate-son-ccr.mp3",
+    cover: "./images/fortunate-son.jfif",
+    color: "#180501",
   },
   {
-    'title':'Rise',
-    'author':'Eddie Vedder and Michael Brook',
-    'src':'./audio/michael-brook-with-eddie-vedder-rise.mp3',
-    'cover':'./images/eddie-vedder.jpg',
-    'color':'#071530'
+    title: "Rise",
+    author: "Eddie Vedder and Michael Brook",
+    src: "./audio/michael-brook-with-eddie-vedder-rise.mp3",
+    cover: "./images/eddie-vedder.jpg",
+    color: "#071530",
   },
   {
-    'title':'Keep me in your heart',
-    'author':'Warren Zevon',
-    'src':'./audio/warren-zevon-keep-me-in-your-heart.mp3',
-    'cover':'./images/warren-zevon.jpg',
-    'color':'#180501'
+    title: "Keep me in your heart",
+    author: "Warren Zevon",
+    src: "./audio/warren-zevon-keep-me-in-your-heart.mp3",
+    cover: "./images/warren-zevon.jpg",
+    color: "#180501",
   },
   {
-    'title':'Elephant gun',
-    'author':'Beirut',
-    'src':'./audio/beirut-elephant-gun.mp3',
-    'cover':'./images/beirut.jpg',
-    'color':'#97BDD2'
+    title: "Elephant gun",
+    author: "Beirut",
+    src: "./audio/beirut-elephant-gun.mp3",
+    cover: "./images/beirut.jpg",
+    color: "#97BDD2",
   },
   {
-    'title':'Fade into you',
-    'author':'Mazzy Star',
-    'src':'./audio/fade-into-you-mazzy-star.mp3',
-    'cover':'./images/mazzy-star.jpg',
-    'color': '#563A58'
+    title: "Fade into you",
+    author: "Mazzy Star",
+    src: "./audio/fade-into-you-mazzy-star.mp3",
+    cover: "./images/mazzy-star.jpg",
+    color: "#563A58",
   },
   {
-    'title':'Scientist',
-    'author':'Coldplay',
-    'src':'./audio/the-scientist-coldplay.mp3',
-    'cover':'./images/coldplay.jpg',
-    'color': '#0E103D'
-  }
+    title: "Scientist",
+    author: "Coldplay",
+    src: "./audio/the-scientist-coldplay.mp3",
+    cover: "./images/coldplay.jpg",
+    color: "#0E103D",
+  },
 ];
 
-const srcs = tracks.map((track) => {return track.src});
+const srcs = tracks.map((track) => {
+  return track.src;
+});
+const body = document.getElementsByTagName("body")[0];
+const main = document.createElement("main");
+const backgroundBody = document.createElement("div");
+backgroundBody.className = "body__background";
+const backgroundBulb = document.createElement("div");
+backgroundBulb.className = "background__bulb";
+const backgroundBulb2 = document.createElement("div");
+backgroundBulb2.className = "background__bulb-2";
+const backgroundBulb3 = document.createElement("div");
+backgroundBulb3.className = "background__bulb-3";
+backgroundBody.append(backgroundBulb, backgroundBulb2, backgroundBulb3);
 
-const body = document.getElementsByTagName('body')[0];
-const main = document.createElement('main');
-
-const backgroundBody = document.createElement('div');
-backgroundBody.className = 'body__background';
-const backgroundBulb = document.createElement('div');
-backgroundBulb.className = 'background__bulb';
-const backgroundBulb2 = document.createElement('div');
-backgroundBulb2.className = 'background__bulb-2';
-const backgroundBulb3 = document.createElement('div');
-backgroundBulb3.className = 'background__bulb-3';
-backgroundBody.append(backgroundBulb,backgroundBulb2,backgroundBulb3);
-
-const player = document.createElement('div');
-player.className = 'container';
+const player = document.createElement("div");
+player.className = "container";
 main.append(player);
 
-const cover = document.createElement('div');
-cover.className = 'cover';
-cover.style.backgroundImage = 'url('+'./images/beirut.jpg'+')';
+const cover = document.createElement("div");
+cover.className = "cover";
+cover.style.backgroundImage = "url(" + "./images/beirut.jpg" + ")";
 
-const volumeBarContainer = document.createElement('div');
-volumeBarContainer.className = 'volume-bar-container';
-const volumeBar = document.createElement('input');
-volumeBar.type = 'range';
-volumeBar.className = 'volume-bar';
+const volumeBarContainer = document.createElement("div");
+volumeBarContainer.className = "volume-bar-container";
+const volumeBar = document.createElement("input");
+volumeBar.type = "range";
+volumeBar.className = "volume-bar";
 volumeBar.value = 70;
-const controlVolume = document.createElement('button');
-controlVolume.className = 'control-volume';
+const controlVolume = document.createElement("button");
+controlVolume.className = "control-volume";
 volumeBarContainer.append(controlVolume, volumeBar);
 cover.append(volumeBarContainer);
 
-
-const trackName = document.createElement('div');
-trackName.className = 'title';
-const trackTitle = document.createElement('p');
-const trackAuthor = document.createElement('p');
-trackTitle.className = 'title__title';
-trackAuthor.className = 'title__author';
+const trackName = document.createElement("div");
+trackName.className = "title";
+const trackTitle = document.createElement("p");
+const trackAuthor = document.createElement("p");
+trackTitle.className = "title__title";
+trackAuthor.className = "title__author";
 trackName.append(trackTitle, trackAuthor);
 
-const playerProgress = document.createElement('div');
-playerProgress.className = 'progress-bar';
-const progressFull = document.createElement('div');
-const progressCurrent = document.createElement('div');
-progressFull.className = 'progress-bar__bar-full';
-progressCurrent.className = 'progress-bar__bar-current';
-const progressRange = document.createElement('INPUT');
-progressRange.setAttribute('type','range');
-progressRange.classList = 'input-range';
-const timeFull = document.createElement('p');
-const timeCurrent = document.createElement('p');
-timeFull.className = 'progress-bar__time-full';
-timeCurrent.className = 'progress-bar__time-current';
+const playerProgress = document.createElement("div");
+playerProgress.className = "progress-bar";
+const progressFull = document.createElement("div");
+const progressCurrent = document.createElement("div");
+progressFull.className = "progress-bar__bar-full";
+progressCurrent.className = "progress-bar__bar-current";
+const progressRange = document.createElement("INPUT");
+progressRange.setAttribute("type", "range");
+progressRange.classList = "input-range";
+const timeFull = document.createElement("p");
+const timeCurrent = document.createElement("p");
+timeFull.className = "progress-bar__time-full";
+timeCurrent.className = "progress-bar__time-current";
 progressFull.append(progressCurrent, progressRange);
 playerProgress.append(timeFull, progressFull, timeCurrent);
 
-const playerControls = document.createElement('div');
-playerControls.className = 'controls';
-const controlPlayPause = document.createElement('button');
-const controlPrev = document.createElement('button');
-const controlNext = document.createElement('button');
-const controlRepeatShuffle = document.createElement('button');
-const controlPlaylist = document.createElement('button');
-controlPlayPause.className = 'control control-play';
-controlPlaylist.className = 'control control-playlist';
-controlRepeatShuffle.className = 'control control-repeat';
-controlNext.className = 'control control-right';
-controlPrev.className = 'control control-left';
-playerControls.append(controlRepeatShuffle, controlPrev, controlPlayPause, controlNext, controlPlaylist);
+const playerControls = document.createElement("div");
+playerControls.className = "controls";
+const controlPlayPause = document.createElement("button");
+const controlPrev = document.createElement("button");
+const controlNext = document.createElement("button");
+const controlRepeatShuffle = document.createElement("button");
+const controlPlaylist = document.createElement("button");
+controlPlayPause.className = "control control-play";
+controlPlaylist.className = "control control-playlist";
+controlRepeatShuffle.className = "control control-repeat";
+controlNext.className = "control control-right";
+controlPrev.className = "control control-left";
+playerControls.append(
+  controlRepeatShuffle,
+  controlPrev,
+  controlPlayPause,
+  controlNext,
+  controlPlaylist
+);
 
-let music = new Audio;
-music.preload = 'auto';
+let music = new Audio();
+music.preload = "auto";
 
 const getCurrentTime = (value) => {
   let sec = Math.floor(value);
   let min = Math.floor(sec / 60);
   sec -= min * 60;
-  return `${min}:${(sec < 10) ? '0' + sec : sec}`;
-}
+  return `${min}:${sec < 10 ? "0" + sec : sec}`;
+};
 
-const playlistContainer = document.createElement('div');
-playlistContainer.className = 'playlist';
-const playlistButton = document.createElement('button');
-playlistButton.className = 'playlist__button';
-const playlistList = document.createElement('ul');
-playlistList.className = 'playlist__list';
+const playlistContainer = document.createElement("div");
+playlistContainer.className = "playlist";
+const playlistButton = document.createElement("button");
+playlistButton.className = "playlist__button";
+const playlistList = document.createElement("ul");
+playlistList.className = "playlist__list";
 playlistContainer.append(playlistButton, playlistList);
 
-player.append(cover, trackName, playerProgress, playerControls, playlistContainer);
+player.append(
+  cover,
+  trackName,
+  playerProgress,
+  playerControls,
+  playlistContainer
+);
 
-controlPlaylist.addEventListener('click', () => {
-  playlistContainer.classList.add('playlist-open');
+controlPlaylist.addEventListener("click", () => {
+  playlistContainer.classList.add("playlist-open");
 });
 
-playlistButton.addEventListener('click', () => {
-  playlistContainer.classList.remove('playlist-open');
+playlistButton.addEventListener("click", () => {
+  playlistContainer.classList.remove("playlist-open");
 });
 
-body.addEventListener('click', (event) => {
-    if (!controlPlaylist.contains(event.target) && !playlistButton.contains(event.target) && !playlistContainer.contains(event.target)) {
-      playlistContainer.classList.remove('playlist-open');
-    }
+body.addEventListener("click", (event) => {
+  if (
+    !controlPlaylist.contains(event.target) &&
+    !playlistButton.contains(event.target) &&
+    !playlistContainer.contains(event.target)
+  ) {
+    playlistContainer.classList.remove("playlist-open");
+  }
 });
 
-const footer = document.createElement('footer');
-footer.className = 'footer';
-const footerContainer = document.createElement('div');
-footerContainer.className = 'footer__container';
-const footerYear = document.createElement('div');
-const footerAuthor = document.createElement('a');
-const footerRSS = document.createElement('a');
-footerYear.className = 'footer__year';
-footerYear.textContent = '2023';
-footerAuthor.className = 'footer__author';
-footerAuthor.textContent = 'Alv0425';
-footerAuthor.href = 'https://github.com/Alv0425';
-footerRSS.className = 'footer__rss-logo';
-footerRSS.href = 'https://rs.school/js-stage0/';
+const footer = document.createElement("footer");
+footer.className = "footer";
+const footerContainer = document.createElement("div");
+footerContainer.className = "footer__container";
+const footerYear = document.createElement("div");
+const footerAuthor = document.createElement("a");
+const footerRSS = document.createElement("a");
+footerYear.className = "footer__year";
+footerYear.textContent = "2023";
+footerAuthor.className = "footer__author";
+footerAuthor.textContent = "Alv0425";
+footerAuthor.href = "https://github.com/Alv0425";
+footerRSS.className = "footer__rss-logo";
+footerRSS.href = "https://rs.school/js-stage0/";
 footerContainer.append(footerYear, footerAuthor, footerRSS);
 footer.append(footerContainer);
 
-// url(./assets/angle-down.svg) 
-//            url(./assets/caret-right-solid.svg)
-//            url(./assets/left.svg)
-//            url(./assets/pause.svg)
-//            url(./assets/play.svg)
-//            url(./assets/shuffle-solid.svg)
-//            url(./assets/right.svg)
-//            url(./assets/repeat-solid.svg)
-//            url(./assets/playlist.svg)
-//            url(./assets/rss.svg)
-//            url(./assets/volume.svg)
-//            url(./assets/volume-muted.svg)
-
 const icons = [
-  './assets/angle-down.svg',
-  './assets/caret-right-solid.svg',
-  './assets/left.svg',
-  './assets/pause.svg',
-  './assets/play.svg',
-  './assets/shuffle-solid.svg',
-  './assets/right.svg',
-  './assets/repeat-solid.svg',
-  './assets/playlist.svg',
-  './assets/rss.svg',
-  './assets/volume.svg,',
-  './assets/volume-muted.svg'
+  "./assets/angle-down.svg",
+  "./assets/caret-right-solid.svg",
+  "./assets/left.svg",
+  "./assets/pause.svg",
+  "./assets/play.svg",
+  "./assets/shuffle-solid.svg",
+  "./assets/right.svg",
+  "./assets/repeat-solid.svg",
+  "./assets/playlist.svg",
+  "./assets/rss.svg",
+  "./assets/volume.svg",
+  "./assets/volume-muted.svg",
 ];
 
-document.addEventListener("DOMContentLoaded", () => {
-  body.prepend(backgroundBody,main,footer);
+function loadImage(src) {
+  let img = new Image();
+  img.src = src;
+}
+
+icons.forEach((iconSrc) => {
+  loadImage(iconSrc);
 });
 
+tracks.forEach((track) => {
+  loadImage(track.cover);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  body.prepend(backgroundBody, main, footer);
+});
