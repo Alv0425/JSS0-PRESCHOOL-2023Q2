5aa0e40d08b74f6ad227c60fd2df7cd667062ea7
Alv0425 <vilejshikova@gmail.com>
2023-09-29 23:42:07 +0300

    feat: add handlers for click on filter-menu button

--- image-gallery/js/gallery.js
+++ image-gallery/js/gallery.js
@@ -1,1 +1,14 @@
 'use strict';
+filterButton.addEventListener('click', () => {
+  filterButton.classList.toggle('header__filter-button-rot');
+  searchFilter.classList.toggle('search__filter-show');
+  body.classList.toggle('body-lock');
+});
+
+document.addEventListener('click', (event) => {
+  if (!filterButton.contains(event.target) && !searchFilter.contains(event.target) && !searchContainer.contains(event.target)){
+    filterButton.classList.remove('header__filter-button-rot');
+    searchFilter.classList.remove('search__filter-show');
+    body.classList.remove('body-lock');
+  }
+});
