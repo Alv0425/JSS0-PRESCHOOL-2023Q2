32ad1b2181dcca522c86de9f6003c21bd66c4069
Alv0425 <vilejshikova@gmail.com>
2023-10-11 16:52:10 +0300

    feat: add game cless ang method for generation of color dist

--- random-game/js/game.js
+++ random-game/js/game.js
@@ -10,3 +10,31 @@ function removeChilds(element) {
       element.removeChild(element.firstChild);   
   }
 }
+
+class Game {
+  constructor(level) {
+    this.level = level;
+    this.tubesColors = [];
+    this.tubes = [];
+    this.steps = 0;
+  }
+
+  generateLiquid(){
+    let tubes = [];
+    let num = 2*this.level;
+    for (let i = 0; i < num + 1; i++){
+      let color = COLORS[i];
+      tubes.push([color,color,color,color]);
+    }
+    let layers = [];
+    for (let i = 0; i < 4; i++){
+      layers.push(tubes.map((tube) => tube[0]).sort(() => Math.random() - 0.5));
+    }
+    tubes = tubes.map((tube, index) =>{ 
+      tube = layers.map((layer) => layer[index]);
+      return tube
+    });
+    this.tubesColors = tubes;
+  }
+
+}
